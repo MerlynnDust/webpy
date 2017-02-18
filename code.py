d3936b5af42c18ddbc3e8e51632f11d695213a1b
MerlynnDust <Anthony.angel@x2i.systems>
2017-02-18 13:09:33 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,9 +2,18 @@ import web
 
 render = web.template.render('templates/')
 
+
+
+web.config.smtp_server = 'smtp.gmail.com'
+web.config.smtp_port = 587
+web.config.smtp_username = 'cookbook@gmail.com'
+web.config.smtp_password = 'secret'
+web.config.smtp_starttls = True
+
+
 urls = (
     '/', 'index'
-
+    '/add','add'
 )
 
 class index:
@@ -12,12 +21,10 @@ class index:
         name = 'Bob'
         return render.index(name)
 
-
-#testing around with this class, not sure if this is the right direction
 class add:
-    def POST(self):
-        i = web.input()
-        n = db.insert('todo', title=i. title)
+    def ADD(self):
+        message = web.input()
+        web.sendmail('cookbook@gmail.com', 'email', 'subject', 'message')
 
 
 if __name__ == "__main__":
